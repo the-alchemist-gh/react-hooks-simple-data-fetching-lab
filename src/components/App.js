d9a7461301c900b26459c9cabb512370a3694600
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-08-02 08:26:47 +0000

    Assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React,{useEffect, useState} from "react";
+
+
+function App(){
+  const[image, setImage] = useState("")
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then((r) => setImage(r.message))
+  },[])
+
+  return (
+    <div>
+      {
+        (image!=="")?<img src={image} alt="A Random Dog"/> : <p>Loading...</p>
+      }
+    </div>
+    
+    
+  )
+}
+
+export default App;
